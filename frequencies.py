cda9306a3667008bf2d2e91714c61dc9b9b135cb
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-10-02 17:22:37 +0100

    Completed task

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in items:
+        str = string(items[i])
+        if(frequencies[str] == None):
+            frequencies[str] = 1;
+        else:
+            frequencies[str] = frequencies[str] + 1;
+    
     return frequencies
